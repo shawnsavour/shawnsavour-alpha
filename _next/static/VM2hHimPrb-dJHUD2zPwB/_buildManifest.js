a49cd0a0204e0fec38b274b47eca6e2d77ba3e5a
Shawn <sondc@vmodev.com>
2023-06-28 22:33:50 +0700

    [Shawn] update code without message

---
+++ _next/static/VM2hHimPrb-dJHUD2zPwB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/32586a8b0a8a958d.css","static/chunks/pages/index-bbb4d197df121ebf.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
